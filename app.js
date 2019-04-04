7e1ff115e67f3cc303cc5f5c32fed80e39d546c8
udom19 <udomcosmas@gmail.com>
2019-04-03 21:55:28 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+
+var publicDir = require('path').join(__dirname,'/public');
+
+app.set("view engine", "ejs");
+app.use(express.static(publicDir));
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/etgehubteens", function(req, res){
+  res.render("etgehubteens");
+});
+
+app.get("/parents", function(req, res){
+  res.render("parents");
+});
+
+app.get("/educators", function(req, res){
+  res.render("educators");
+});
+
+app.get("/web", function(req, res){
+  res.render("web");
+});
+
+app.post("/contact", function(req, res){
+  res.render("contact.ejs");
+});
+
+app.get("/etgehubteens/juniors", function(req, res){
+  res.render("juniors");
+});
+
+app.get("/etgehubteens/elementary", function(req, res){
+  res.render("elementary");
+});
+
+app.get("/etgehubteens/intermediate", function(req, res){
+  res.render("intermediate");
+});
+
+app.get("/etgehubteens/advance", function(req, res){
+  res.render("advance");
+});
+
+
+app.listen("5000", function(){
+  console.log("The Server has started");
+}); 
